07a685f8b40d26172759ed60f93db4a9433460ef
Julian Simioni <julian@simioni.org>
2018-03-30 22:56:31 -0400

    Remove hack temporarily needed to work around java download issues

--- elasticsearch/packer/scripts/java.sh
+++ elasticsearch/packer/scripts/java.sh
@@ -7,16 +7,4 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 
-# hack to fix java download issues
-# https://ubuntuforums.org/showthread.php?t=2374686&page=4&p=13731177#post13731177
-# https://stackoverflow.com/questions/48329277/installing-oracle-8-jdk-on-ubuntu
-sudo apt-get install -y oracle-java8-installer || true
-cd /var/lib/dpkg/info
-sudo ls
-sudo sed -i 's|JAVA_VERSION=8u151|JAVA_VERSION=8u162|' oracle-java8-installer.*
-sudo sed -i 's|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/|' oracle-java8-installer.*
-sudo sed -i 's|SHA256SUM_TGZ="c78200ce409367b296ec39be4427f020e2c585470c4eed01021feada576f027f"|SHA256SUM_TGZ="68ec82d47fd9c2b8eb84225b6db398a72008285fafc98631b1ff8d2229680257"|' oracle-java8-installer.*
-sudo sed -i 's|J_DIR=jdk1.8.0_151|J_DIR=jdk1.8.0_162|' oracle-java8-installer.*
-## end hack
-
 sudo apt-get install -y oracle-java8-installer
