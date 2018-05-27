478b121c43f35a8d23fd321e7e88e8862e5d2298
Julian Simioni <julian@simioni.org>
2018-05-27 16:07:18 -0400

    feat(packer): Add scripts/udev rules for easy NVMe configuration
    The name of the block devices for EBS volumes on older EC2 isntances is
    different than on C5 or M5 instances with NVMe volumes.
    
    By adding udev rules with some helper scripts, symlinks which in all
    cases symlink EBS volumes to the standard /dev/sda, /dev/sdb, etc can be
    created.
    
    This makes it quite simple to support any EC2 instance type.

--- elasticsearch/packer/scripts/default.sh
+++ elasticsearch/packer/scripts/default.sh
@@ -13,3 +13,11 @@ root soft nofile 128000\n
 root hard nofile 128000" | sudo tee --append /etc/security/limits.conf
 
 echo "fs.file-max = 500000" | sudo tee --append /etc/sysctl.conf
+
+# move udev rules and related scripts to the proper place
+sudo mv /tmp/ebsnvme-id /sbin
+sudo mv /tmp/ec2udev-vbd /sbin
+sudo chown root:root /sbin/ebsnvme-id /sbin/ec2udev-vbd
+sudo chmod u+x /sbin/ebsnvme-id /sbin/ec2udev-vbd
+sudo chmod go-rwx /sbin/ebsnvme-id /sbin/ec2udev-vbd
+sudo mv /tmp/10-aws.rules /etc/udev/rules.d
